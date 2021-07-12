bcfdd01368c43f254ab5f5721224ff78e3fd1241
ducbl98 <ductit1998@gmail.com>
2021-07-12 11:35:41 +0700

    Test3

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rectangle_1 = require("./rectangle");
+let rext = new rectangle_1.Rectangle(5, 5, 10, 20);
+console.log(rext.toString());
+console.log(rext.area());
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import {Rectangle} from "./rectangle";
+
+let rext =new Rectangle(5,5,10,20);
+console.log(rext.toString());
+console.log(rext.area());
